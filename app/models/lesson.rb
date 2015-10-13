5541ab5912dbcc2348856ad4c4bfa7fd15daec08
andrew dang & brian lau <student@epicodus.com>
2015-10-13 16:18:39 -0700

    add previous and next

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,16 @@ class Lesson < ActiveRecord::Base
   belongs_to :chapter
 
   default_scope { order('number ASC')}
+
+  def next
+    if self.number
+      Lesson.where(:number => (self.number + 1),:chapter_id => self.chapter_id).first
+    end
+  end
+
+  def previous
+    if self.number
+      Lesson.where(:number => (self.number - 1),:chapter_id => self.chapter_id).first
+    end
+  end
 end
